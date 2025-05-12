c4afadbcb79c36ebf28d427e93c22c7a1df9b803
foxwarriors <adi_ciuca@yahoo.com>
2025-05-12 22:26:22 +0300

    tryfixes 6

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-/* Manifest version: bxnJWNNI */
+/* Manifest version: fMhSCYgP */
 // Caution! Be sure you understand the caveats before publishing an application with
 // offline support. See https://aka.ms/blazor-offline-considerations
 
