befda2448579e479cb909a87f5f804c7470c21f2
foxwarriors <adi_ciuca@yahoo.com>
2025-05-12 21:51:59 +0300

    3

--- service-worker-assets.js
+++ service-worker-assets.js
@@ -1,5 +1,5 @@
 self.assetsManifest = {
-  "version": "2IVkfhnK",
+  "version": "vWpuv7WP",
   "assets": [
     {
       "hash": "sha256-Plp+mPd9DjNE9J+PbDxw2Rt5BD08cdFGhAvdbA4G0x4=",
@@ -278,7 +278,7 @@ self.assetsManifest = {
       "url": "icon-512.png"
     },
     {
-      "hash": "sha256-xTWHFAU10Qf8/tJuJ9ODF09txhMMKMLBZOaop2FxZNw=",
+      "hash": "sha256-yCiBAmDDPvdGNkfHhJpQO8sN+VOUppQNlxPNmJ3O80M=",
       "url": "index.html"
     },
     {
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-/* Manifest version: 2IVkfhnK */
+/* Manifest version: vWpuv7WP */
 // Caution! Be sure you understand the caveats before publishing an application with
 // offline support. See https://aka.ms/blazor-offline-considerations
 
